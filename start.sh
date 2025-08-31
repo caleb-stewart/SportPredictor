17703f7ed85f0eeac63a10a606e24deda72326a3
caleb-stewart <calebstew32@hotmail.com>
2025-08-31 15:17:19 -0700

    setting up environment on linux

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ cleanup() {
   echo "[BASH] Stopping all servers..."
   # kill all background jobs started by this script
   kill $(jobs -p) 2>/dev/null
+  deactivate
 }
 
 # Trap EXIT signal (script ending) to run cleanup
@@ -14,6 +15,7 @@ echo "[BASH] Starting Rails server on port 3141..."
 rails s -p 3141 &
 
 echo "[BASH] Starting Flask server on port 2718..."
+source PredictorFlask/pf-venv/bin/activate
 python3 PredictorFlask/app.py &
 
 echo "[BASH] Starting Vue app on port 5173..."
